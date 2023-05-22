155a65c0fccd036c7ce44b0fefe2b2e219197e54
Jeff Wang <cjw39@hotmail.com>
2023-05-22 00:11:24 -0700

    Initial commit.

---
+++ src/app.jsx
@@ -0,0 +1,187 @@
+import React, {useState} from 'react';
+
+/**
+ * The browser local storage object.
+ */
+const localStorage = window.localStorage;
+
+/**
+ * The local storage name.
+ */
+const storageKey = 'spending-state';
+
+/**
+ * The main app component.
+ */
+export default function App() {
+
+  /**
+   * The app state object.
+   */
+  let [state, setState] = useState(
+    localStorage.getItem(storageKey) ? JSON.parse(localStorage.getItem(storageKey)) : {entries: []}
+  );
+
+  /**
+   * The error text to show on the UI.
+   */
+  let [error, setError] = useState();
+
+  /**
+   * Adds a new transaction entry.
+   *
+   * @param {number} value The transaction value
+   */
+  function onAddEntry(value) {
+    if (!value || typeof value !== 'number')
+      return;
+
+    setError();
+
+    let entries = [...state.entries];
+
+    // We only keep 20 entries
+    if (entries.length === 20) {
+      entries.pop();
+    }
+
+    // Add the new entry
+    entries.splice(0, 0, {
+      time: Date.now(),
+      value: value,
+      total: entries.length === 0 ? value : entries[0].total + value
+    });
+
+    let newState = {
+      entries: entries
+    };
+
+    try {
+      localStorage.setItem(storageKey, JSON.stringify(newState));
+      setState(newState);
+    }
+    catch (error) {
+      setError(JSON.stringify(error));
+    }
+  }
+
+  return (
+    <div>
+      {error && <div>{error}</div>}
+      <Total entries={state.entries}/>
+      <Entries entries={state.entries}/>
+      <Inputs onAddEntry={onAddEntry}/>
+    </div>
+  )
+}
+
+/**
+ * The UI component that shows the total amount.
+ *
+ * @param {array} entries The prop that contains the transaction entries.
+ */
+function Total({entries}) {
+  let total = entries && entries.length > 0 ? entries[0].total : 0
+  return (<h1>${total}</h1>)
+}
+
+/**
+ * The UI component that shows the list of transaction entries.
+ *
+ * @param {array} entries The prop that contains the transaction entries.
+ */
+function Entries({entries}) {
+  return (
+    <table
+      style={{
+        width: '100%'
+      }}
+    >
+      <tbody>
+        {entries.map(entry => <Entry entry={entry}/>)}
+      </tbody>
+    </table>
+  )
+}
+
+/**
+ * The UI component for a single transaction entry.
+ *
+ * @param {object} entry The transaction object.
+ */
+function Entry({entry}) {
+  return (
+    <tr>
+      <td>
+        {(new Date(entry.time)).toLocaleString('en-us', {dateStyle: 'full'})}
+      </td>
+      <td
+        style={{
+          textAlign: 'right'
+        }}
+      >{entry.value}</td>
+      <td
+        style={{
+          textAlign: 'right'
+        }}
+      >{entry.total}</td>
+    </tr>
+  )
+}
+
+/**
+ * The component to input new transactions.
+ *
+ * @param {function} onAddEntry The callback for a new transaction.
+ */
+function Inputs({onAddEntry}) {
+  let [value, setValue] = useState('');
+
+  return (
+    <div
+      style={{
+        marginTop: '2em',
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        justifyContent: 'space-around'
+      }}
+    >
+      <input
+        type="number"
+        inputMode="decimal"
+        value={value}
+        onChange={event => setValue(parseFloat(event.target.value))}
+        style={{
+          flexGrow: '3'
+        }}
+      />
+
+      <button
+        onClick={() => {
+          onAddEntry(value);
+          setValue('');
+        }}
+        style={{
+          marginLeft: '1rem',
+          flexGrow: '1'
+        }}
+      >
+        Deposit
+      </button>
+
+      <button
+        onClick={() => {
+          onAddEntry(value * -1);
+          setValue('');
+        }}
+        style={{
+          marginLeft: '1rem',
+          flexGrow: '1'
+        }}
+      >
+        Spend
+      </button>
+    </div>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules'
+    ],
+    extensions: ['.js', '.jsx', '.scss']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+}
