0382a5903a9d71cbd44667e8e893f696a2bc57f5
Jeff Wang <cjw39@hotmail.com>
2024-09-05 22:18:24 -0700

    Fastlane snapshot initial setup.

---
+++ ios/App/screenshotUITests/screenshotUITests.swift
@@ -0,0 +1,48 @@
+//
+//  screenshotUITests.swift
+//  screenshotUITests
+//
+//  Created by Wang, Chih Jeff on 9/3/24.
+//
+
+import XCTest
+
+final class screenshotUITests: XCTestCase {
+
+    @MainActor override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        XCUIDevice.shared.orientation = .portrait
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    @MainActor func testExample() throws {
+        // UI tests must launch the application that they test.
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        app.webViews.webViews.webViews/*@START_MENU_TOKEN@*/.buttons["Add entry"]/*[[".otherElements[\"Allowly\"].buttons[\"Add entry\"]",".buttons[\"Add entry\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+//        let key = app/*@START_MENU_TOKEN@*/.keys["2"]/*[[".keyboards.keys[\"2\"]",".keys[\"2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+//        key.tap()
+//        key.tap()
+//        app/*@START_MENU_TOKEN@*/.keys["0"]/*[[".keyboards.keys[\"0\"]",".keys[\"0\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        app/*@START_MENU_TOKEN@*/.keys["2"]/*[[".keyboards.keys[\"2\"]",".keys[\"2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.keys["0"]/*[[".keyboards.keys[\"0\"]",".keys[\"0\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        snapshot("MainScreen")
+    }
+}
